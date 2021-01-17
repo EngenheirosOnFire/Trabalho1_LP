6a49f38c34e92a47f502827d7dd8c247211acb79
Roberto Barreto <a21123@alunos.ipca.pt>
2021-01-17 12:12:28 +0000

    Merge branch 'Roberto'

--- Doc/html/search/searchdata.js
+++ Doc/html/search/searchdata.js
@@ -1,33 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_acdefilmnopstv",
-  1: "i",
-  2: "fms",
-  3: "aelmnotv",
-  4: "acdenpt",
-  5: "dept",
-  6: "_"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "typedefs",
-  6: "defines"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Typedefs",
-  6: "Macros"
 };
 
